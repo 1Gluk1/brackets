11fda43a914fb121ccafb3df0a5b4bf8ba6dd845
Anton Gorbachev <shieldmedia@yandex.ru>
2021-03-15 01:05:55 +0700

    feat: add check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsPairArray = bracketsConfig.map((arr) => arr.join(""));
+  let stack = [];
+  for (let i of str) {
+    stack.push(i);
+    let closeBracket = stack[stack.length - 2] + stack[stack.length - 1];
+    if (bracketsPairArray.indexOf(closeBracket) !== -1) {
+      stack.length = stack.length - 2
+    }
+  }
+  return stack.length === 0;
 }
